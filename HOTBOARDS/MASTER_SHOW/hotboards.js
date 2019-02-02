8b8459c1c00047f5574a9808a849c6b50acb0f99
brianaruti <baruti@gmail.com>
2019-02-01 23:45:36 -0500

    Character count function completed

--- HOTBOARDS/MASTER_SHOW/hotboards.js
+++ HOTBOARDS/MASTER_SHOW/hotboards.js
@@ -1,27 +1,49 @@
-
-let titleCharCount = 50;
-let subTitleCharcount = 30;
-
+//Charcater counts used in the document
+const characterCountDict = {
+  boardTitle: 50,
+  boardSubTitle: 30
+};
 
 //Document has loaded
-$(document).ready(function () {
+$(document).ready(function() {
+  // vizrt.payloadhosting.setFieldValueCallbacks(createCallbacks());
+  //vizrt.payloadhosting.initialize();
 
-    // vizrt.payloadhosting.setFieldValueCallbacks(createCallbacks());
-    vizrt.payloadhosting.initialize();
+  $(".charCount").on("keyup", function() {
+    $(this).css("background-color", "lightblue");
 
-    //Key up event for input fields with character counts
-    $(".charCount").keyup(function () {
-        $(this).css("background-color", "lightblue");
-        var charLength = $("#boardTitle").val().length;
-        CharacterCounter(this.id,charLength);
-    });
+    CharacterCounter(this);
+  });
 
-    //Function to set characters remaining 
-    function CharacterCounter(id, length){
-
-        alert(length);
-
-    };
-
+  
 });
 
+//Function to set characters remaining, gets the element from the keyup event
+function CharacterCounter(e) {
+  let maxNum = characterCountDict[e.id]; //get max character count
+
+  //Set the maxLength attribute for the field triggering the event
+  if ($(e).attr("maxLength") != maxNum) {
+    $(e).attr("maxLength", maxNum);
+  }
+
+  //Get the text length
+  var charLength = $(e).val().length;
+
+  if (charLength > maxNum) {
+    $(e).css("background-color", "red");
+    $(e).css("color", "white");
+  } else {
+    $(e).css("background-color", "lightblue");
+    $(e).css("color", "black");
+  }
+
+  if(charLength === 0){
+    e.nextElementSibling.innerHTML = "";
+  }else{
+    e.nextElementSibling.innerHTML = "REMAING CHARACTERS: " + (maxNum - charLength);
+
+  }
+ 
+  // alert(maxNumber);
+}
